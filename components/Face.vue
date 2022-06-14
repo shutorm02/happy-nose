22f5d2c94b3e4fc52c6f7526b517e7555a814369
shutorm02 <shutorm2106@gmail.com>
2022-06-14 13:40:50 +0900

    Improve: 犬のイラストを顔と足に分解、コンポーネント化

---
+++ components/Face.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <svg width="218" height="302" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="eyes">
+        <circle cx="46.1179" cy="172.227" r="13.5" fill="#C4C4C4"/>
+        <circle cx="174.118" cy="172.227" r="13.5" fill="#C4C4C4"/>
+      </g>
+      <g id="nose">
+        <path d="M106.972 253.788C108.797 255.348 111.473 255.39 113.347 253.888L141.618 231.227C144.621 228.036 142.428 222.727 138.046 222.727H84.1667C79.5208 222.727 77.3866 228.51 80.9187 231.528L106.972 253.788Z" fill="#C4C4C4"/>
+        <ellipse cx="110.618" cy="226.727" rx="32" ry="13" fill="#C4C4C4"/>
+      </g>
+      <g id="muzzle">
+        <line x1="110.648" y1="239.727" x2="110.648" y2="277.841" stroke="#C4C4C4" stroke-width="3"/>
+        <line x1="110.057" y1="275.985" x2="78.5269" y2="300.008" stroke="#C4C4C4" stroke-width="3"/>
+        <line x1="111.483" y1="276.501" x2="143.867" y2="299.361" stroke="#C4C4C4" stroke-width="3"/>
+      </g>
+      <g id="ears">
+        <g id="left-ear">
+          <path d="M63.1256 41.5065C49.5478 38.1124 25.6489 4.02984 25.6489 4.02984" stroke="#C4C4C4" stroke-width="3"/>
+          <path d="M19.565 46.6179C10.8021 36.9851 25.2219 2.77726 25.2219 2.77726" stroke="#C4C4C4" stroke-width="3"/>
+        </g>
+        <g id="right-ear">
+          <path d="M154.022 41.508C167.6 38.1138 191.499 4.03005 191.499 4.03005" stroke="#C4C4C4" stroke-width="3"/>
+          <path d="M197.583 46.6195C206.346 36.9863 191.926 2.77744 191.926 2.77744" stroke="#C4C4C4" stroke-width="3"/>
+        </g>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/ForeFoot.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg width="274" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M71.5051 22C71.5051 22 77.3954 13.9178 48.5331 4.10363C19.6708 -5.7105 2 22 2 22" stroke="#C4C4C4" stroke-width="3"/>
+      <path d="M19.9875 5C19.9875 5 36.1437 8.92308 37.6583 22" stroke="#C4C4C4" stroke-width="3"/>
+      <path d="M45.7267 4C45.7267 4 56.4975 7.92308 57.5072 21" stroke="#C4C4C4" stroke-width="3"/>
+      <path d="M202.495 23C202.495 23 196.605 14.9178 225.467 5.10363C254.329 -4.7105 272 23 272 23" stroke="#C4C4C4" stroke-width="3"/>
+      <path d="M254.012 6C254.012 6 237.856 9.92308 236.342 23" stroke="#C4C4C4" stroke-width="3"/>
+      <path d="M228.273 5C228.273 5 217.502 8.92308 216.493 22" stroke="#C4C4C4" stroke-width="3"/>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
